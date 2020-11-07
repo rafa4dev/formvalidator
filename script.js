8c0518f599180951f0a51cf8a5c1ef818774d534
Rafael Paz <dudex1992@gmail.com>
2020-11-07 14:33:53 -0300

    nova regra para validar email

--- script.js
+++ script.js
@@ -39,6 +39,14 @@ let myValidator = {
                             return 'Campo tem que ter pelo menos '+rDetails[1]+' caracters';
                         }
                         break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail digitado não é valido!';
+                            }
+                        }
+                        break;
                 }
             }
         }
